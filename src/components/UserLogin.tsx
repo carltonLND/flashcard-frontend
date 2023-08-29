d60361f3d85a8f056c80573b72601cefebd71e14
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-08-29 12:32:57 +0100

    Added user login component

--- src/components/UserLogin.tsx
+++ src/components/UserLogin.tsx
@@ -1,9 +1,78 @@
+import { useEffect, useState } from "react";
+import { User } from "../types/IUser";
+import {
+  Box,
+  Button,
+  Center,
+  Divider,
+  HStack,
+  Input,
+  Select,
+} from "@chakra-ui/react";
+
 interface UserLoginProps {
   setUser: (name: string) => void;
 }
 
 function UserLogin({ setUser }: UserLoginProps) {
-  return <></>;
+  const [userList, setUserList] = useState<User[]>([]);
+  const [selectedUser, setSelectedUser] = useState("");
+  const [newUserInput, setNewUserInput] = useState("");
+
+  const handleNewUserSubmit = async () => {
+    // IF newUserInput is not an empty string
+    if (newUserInput === "") return;
+    // Send a request
+    // IF response status is 201
+    // THEN
+    setUser(newUserInput);
+  };
+
+  useEffect(() => {
+    // TODO: Fetch the users from the backend
+    setUserList(["Carlton", "Ana", "Neill", "Beth"]);
+  }, []);
+
+  return (
+    <HStack justifyContent={"space-evenly"} height={"100vh"}>
+      <Box
+        display={"flex"}
+        flexDirection={"column"}
+        justifyContent={"center"}
+        alignItems={"center"}
+        gap={"1em"}
+      >
+        <Select onChange={(e) => setSelectedUser(e.target.value)}>
+          <option selected disabled value="">
+            Select User
+          </option>
+          {userList.map((u, i) => (
+            <option key={i} value={u}>
+              {u}
+            </option>
+          ))}
+        </Select>
+        <Button onClick={() => setUser(selectedUser)}>LOGIN</Button>
+      </Box>
+      <Center height="50%">
+        <Divider orientation="vertical" />
+      </Center>
+      <Box
+        display={"flex"}
+        flexDirection={"column"}
+        justifyContent={"center"}
+        alignItems={"center"}
+        gap={"1em"}
+      >
+        <Input
+          onChange={(e) => setNewUserInput(e.target.value.slice(0, 20))}
+          value={newUserInput}
+          placeholder="Your name..."
+        />
+        <Button onClick={handleNewUserSubmit}>SIGNUP</Button>
+      </Box>
+    </HStack>
+  );
 }
 
 export default UserLogin;
