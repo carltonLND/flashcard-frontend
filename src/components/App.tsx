c15066293076ca1dce3ce513bd2fca7a141aaa51
Ana Rebelo <anagmrebelo@hotmail.com>
2023-08-31 13:12:39 +0100

    Update user state to user object

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,18 +1,19 @@
 import { Button } from "@chakra-ui/react";
 import UserLogin from "./UserLogin";
 import { useState } from "react";
+import { User } from "../types/IUser";
 
 function App() {
-  const [user, setUser] = useState("");
+  const [user, setUser] = useState<User | null>(null);
 
   return (
     <>
-      {user === "" ? (
+      {!user ? (
         <UserLogin setUser={setUser} />
       ) : (
         <>
-          <h1>WE ARE LOGGED IN AS {user}</h1>
-          <Button onClick={() => setUser("")}>LOGOUT</Button>
+          <h1>WE ARE LOGGED IN AS {user.name}</h1>
+          <Button onClick={() => setUser(null)}>LOGOUT</Button>
         </>
       )}
     </>
--- src/components/UserLogin.tsx
+++ src/components/UserLogin.tsx
@@ -11,12 +11,12 @@ import {
 } from "@chakra-ui/react";
 
 interface UserLoginProps {
-  setUser: (name: string) => void;
+  setUser: (obj: User | null) => void;
 }
 
 function UserLogin({ setUser }: UserLoginProps) {
   const [userList, setUserList] = useState<User[]>([]);
-  const [selectedUser, setSelectedUser] = useState("");
+  const [selectedUserId, setSelectedUserId] = useState<number>();
   const [newUserInput, setNewUserInput] = useState("");
 
   const handleNewUserSubmit = async () => {
@@ -24,7 +24,7 @@ function UserLogin({ setUser }: UserLoginProps) {
 
     const response = await server.post("/users", { name: newUserInput });
     if (response.status === 201) {
-      setUser(newUserInput);
+      setUser(response.data);
     }
   };
 
@@ -40,17 +40,26 @@ function UserLogin({ setUser }: UserLoginProps) {
   return (
     <HStack justifyContent={"space-evenly"} height={"100vh"}>
       <Center flexDirection={"column"} gap={"1em"}>
-        <Select onChange={(e) => setSelectedUser(e.target.value)}>
-          <option selected disabled value="">
+        <Select
+          onChange={(e) => setSelectedUserId(parseInt(e.target.value))}
+          defaultValue={"-1"}
+        >
+          <option disabled value="-1">
             Select User
           </option>
           {userList.map((user) => (
-            <option key={user.id} value={user.name}>
+            <option key={user.id} value={user.id}>
               {user.name}
             </option>
           ))}
         </Select>
-        <Button onClick={() => setUser(selectedUser)}>LOGIN</Button>
+        <Button
+          onClick={() =>
+            setUser(userList.find((user) => user.id === selectedUserId) as User)
+          }
+        >
+          LOGIN
+        </Button>
       </Center>
       <Center height="50%">
         <Divider orientation="vertical" />
